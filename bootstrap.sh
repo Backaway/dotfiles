2fceae36437a363f1eb751cc9c0ee6ffd5a7b39b
whz <junevimer@gmail.com>
2014-03-17 15:31:57 +0800

    feature:add muttrc and msmtprc for sendemail

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 echo "init:" > Makefile
 echo "\t\tln -fs `pwd`/gitconfig ~/.gitconfig" >> Makefile
 echo "\t\tln -fs `pwd`/bashrc ~/.bashrc" >> Makefile
+echo "\t\tln -fs `pwd`/msmtprc ~/.msmtprc" >> Makefile
+echo "\t\tln -fs `pwd`/muttrc ~/.muttrc" >> Makefile
 
 # initialize your dotfile env
 make init
