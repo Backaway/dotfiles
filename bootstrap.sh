8e148f31f4d08c7950cef78e9648d9613be3c27f
Huizhou Wang <junevimer@gmail.com>
2014-10-30 14:56:52 +0800

    backup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 echo "init:" > Makefile
 echo "\t\tln -fs `pwd`/gitconfig ~/.gitconfig" >> Makefile
-echo "\t\tln -fs `pwd`/bashrc ~/.bashrc" >> Makefile
-echo "\t\tln -fs `pwd`/msmtprc ~/.msmtprc" >> Makefile
-echo "\t\tln -fs `pwd`/muttrc ~/.muttrc" >> Makefile
+echo "\t\tln -fs `pwd`/zshrc ~/.zshrc" >> Makefile
 
 # initialize your dotfile env
 make init
