b345ead746ae147e3213c2427640ed7503a11cc8
Backaway <whz.job@gmail.com>
2015-10-13 14:00:01 +0800

    Feature:merge os x with linux

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,25 @@
-#!/bin/sh
-echo "init:" > Makefile
-echo "\t\tln -fs `pwd`/gitconfig ~/.gitconfig" >> Makefile
-echo "\t\tln -fs `pwd`/svnconfig ~/.subversion/config" >> Makefile
-echo "\t\tln -fs `pwd`/zshrc ~/.zshrc" >> Makefile
+#!/bin/bash
+
 # for solarized theme
 # echo "\t\tln -fs `pwd`/dircolors.solarized ~/.dircolors" >> Makefile
 # for molokai theme
-echo "\t\tln -fs `pwd`/dircolors.molokai ~/.dircolors" >> Makefile
-echo "\t\tln -fs `pwd`/gitignore_global ~/.gitignore_global" >> Makefile
-echo "\t\tln -fs `pwd`/ycm_c_conf.py ~/.ycm_c_conf.py" >> Makefile
-echo "\t\tln -fs `pwd`/ycm_cpp_conf.py ~/.ycm_cpp_conf.py" >> Makefile
-echo "\t\tln -fs `pwd`/ycm_extra_conf.py ~/.ycm_extra_conf.py" >> Makefile
-echo "\t\tln -fs `pwd`/robbyrussell.zsh-theme ~/.oh-my-zsh/themes/robbyrussell.zsh-theme" >> Makefile
-echo "\t\tln -fs `pwd`/wanghuizhou.zsh-theme ~/.oh-my-zsh/themes/wanghuizhou.zsh-theme" >> Makefile
-echo "\t\tln -fs `pwd`/diffwrap.sh /usr/local/bin/diffwrap.sh" >> Makefile
+# ln -fs `pwd`/dircolors.molokai.blue ~/.dircolors
+ln -fs `pwd`/dircolors.molokai.red ~/.dircolors
 
-# initialize your dotfile env
-make init
+# for git
+ln -fs `pwd`/gitconfig ~/.gitconfig
+ln -fs `pwd`/gitignore_global ~/.gitignore_global
+
+# for svn
+mkdir -p ~/.subversion
+ln -fs `pwd`/svnconfig ~/.subversion/config
+sudo ln -fs `pwd`/diffwrap.sh /usr/local/bin/diffwrap.sh
+
+# for zsh
+ln -fs `pwd`/zshrc ~/.zshrc
+ln -fs `pwd`/wanghuizhou.zsh-theme ~/.oh-my-zsh/themes/wanghuizhou.zsh-theme
+
+# for YouCompleteMe
+ln -fs `pwd`/ycm_c_conf.py ~/.ycm_c_conf.py
+ln -fs `pwd`/ycm_cpp_conf.py ~/.ycm_cpp_conf.py
+ln -fs `pwd`/ycm_extra_conf.py ~/.ycm_extra_conf.py
--- ycm_extra_conf.py
+++ ycm_extra_conf.py
@@ -35,47 +35,47 @@ import ycm_core
 # compilation database set (by default, one is not set).
 # CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
-'-Wall',
-'-Wextra',
-'-Werror',
-'-Wc++98-compat',
-'-Wno-long-long',
-'-Wno-variadic-macros',
-'-fexceptions',
-'-DNDEBUG',
-# You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
-# source code needs it.
-# '-DUSE_CLANG_COMPLETER',
-# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-# language to use when compiling headers. So it will guess. Badly. So C++
-# headers will be compiled as C headers. You don't want that so ALWAYS specify
-# a "-std=<something>".
-# For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
-'-std=c++11',
-# '-std=c99',
-# ...and the same thing goes for the magic -x option which specifies the
-# language that the files to be compiled are written in. This is mostly
-# relevant for c++ headers.
-# For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c++',
-# 'c',
-# This path will only work on Linux, but extra paths that don't exist are not
-# harmful
-'-I',
-'.',
-'-isystem',
-'/usr/include',
-'-isystem',
-'/usr/local/include',
+    '-Wall',
+    '-Wextra',
+    '-Werror',
+    '-Wc++98-compat',
+    '-Wno-long-long',
+    '-Wno-variadic-macros',
+    '-fexceptions',
+    '-DNDEBUG',
+    # You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
+    # source code needs it.
+    # '-DUSE_CLANG_COMPLETER',
+    # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
+    # language to use when compiling headers. So it will guess. Badly. So C++
+    # headers will be compiled as C headers. You don't want that so ALWAYS specify
+    # a "-std=<something>".
+    # For a C project, you would set this to something like 'c99' instead of
+    # 'c++11'.
+    '-std=c++11',
+    # '-std=c99',
+    # ...and the same thing goes for the magic -x option which specifies the
+    # language that the files to be compiled are written in. This is mostly
+    # relevant for c++ headers.
+    # For a C project, you would set this to 'c' instead of 'c++'.
+    '-x',
+    'c++',
+    # 'c',
+    # This path will only work on Linux, but extra paths that don't exist are not
+    # harmful
+    '-I',
+    '.',
+    '-isystem',
+    '/usr/include',
+    '-isystem',
+    '/usr/local/include',
 
-# This path will only work on OS X, but extra paths that don't exist are not
-# harmful
-'-isystem',
-'/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../include/c++/v1',
-'-isystem',
-'/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
+    # This path will only work on OS X, but extra paths that don't exist are not
+    # harmful
+    '-isystem',
+    '/Applications/Xcode-beta.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../include/c++/v1',
+    '-isystem',
+    '/Applications/Xcode-beta.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
 ]
 
 
